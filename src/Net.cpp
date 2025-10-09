5d82c95bda73c4a5f961de65f29b605b27e64a60
rosignol08 <rchaffray@gmail.com>
2025-10-09 17:02:07 +0200

    petite update des fonctions

--- src/Net.cpp
+++ src/Net.cpp
@@ -6,26 +6,43 @@ namespace Netlist
 {
     Cell *getCell();
     const std::string &Net::getName()const{
+        return this->name_;
 
-    };
-    unsigned int getId(){
+    }
+    unsigned int Net::getId()const{
+        return this->id_;
         
-    };
-    Term::Type getType(){
+    }
+    Term::Type Net::getType()const{
+        return this->type_;
 
-    };
-    Node *getNode(size_t id){
-
-    };
-    size_t getFreeNodeId(){
-
-    };
-    void add(Node *){
-
-    };
+    }
+    Node *Net::getNode(size_t id)const{
+        return this->nodes_[id];
+    }
+    size_t Net::getFreeNodeId()const{
+        //on doit trouver l'index de la première case libre dans le tableau 
+        //si aucune case n'est libre, elle renverra la taille du tableau,
+        //c'est à dire l'index situé immédiatement après le dernier élément TODO
+        size_t i = 0;
+        while(getNode(i) != nullptr){
+            i++;//pas la bonne façon de faire TODO
+        }/*
+        if()//check si on a une case libre
+        {
+            return this->nodes_[i].noid; l'id de la case
+        }
+        else{
+            return i;
+        }
+            */
+    }
+    void Net::add(Node *){
+        //this->nodes_.push_back(Node*)//faut voir si on doit pas  donner une variable ici et à remove
+    }
     bool Net::remove(Node *){
 
-    };
+    }
     //const std::vector<Node *> &
     //getNodes();
 }
--- src/Term.cpp
+++ src/Term.cpp
@@ -4,42 +4,56 @@ using namespace std;
 
 namespace Netlist{
     bool                    Term::isInternal      ()const{
+        if(this->type_ == 1){
+            return true;
+        }else if(this->type_ == 2)
+        {
+            return false;
+        }
 
-    };
+    }
     bool                    Term::isExternal      ()const{
+     if(this->type_ == 2){
+            return true;
+        }else if(this->type_ == 1)
+        {
+            return false;
+        }
 
-    };
+    }
     const std::string &   Term::getName         () const{
-
-    };
+        return this->name_;
+    }
     Node*                  Term::getNode         (){
-
-    };
-    Net*                   Term::getNet          ()const{};
+        //return this->node_; erreur ici TODO
+    }
+    Net*                   Term::getNet          ()const{
+        return this->net_;
+    }
     Cell*                  Term::getCell         ()const{
-
-    };
+        //return this-> trouver un moyen d'avoir la cell TODO
+    }
     Cell*                  Term::getOwnerCell    ()const{
-
-    };
+        //faut appeler la fontion de net peut etre ?
+    }
     Instance*              Term::getInstance     ()const{
 
-    };
+    }
     Point                   Term::getPosition     ()const{
-
-    };
+        //return this->direction_ je sait pas
+    }
     void                    Term::setNet          ( Net * ){
 
-    };
+    }
     void                    Term::setNet          ( const std::string& ){
 
-    };
+    }
     void                    Term::setPosition     ( const Point& ){
 
-    };
+    }
     void                    Term::setPosition     ( int x , int y ){
 
-    };
+    }
     //Type                    getType         () const ;
     //Direction               getDirection    ();
     //void                    setDirection    ( Direction );
